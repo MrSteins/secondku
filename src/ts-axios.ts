f4a056056c00bd7f77c97e1fea4143a00b6c468a
MrSteins <496053653@qq.com>
2021-07-13 15:43:15 +0800

    init ts-axios

--- src/ts-axios.ts
+++ src/ts-axios.ts
@@ -4,3 +4,18 @@
 export default class DummyClass {
 
 }
+
+interface Clock {
+  currentTime: Date
+}
+
+interface Clocks {
+  currentTime: Date
+  name:string
+}
+
+const clock1:Clocks = {
+  currentTime: new Date(),
+  name: 'hx'
+}
+const clock2: Clock = clock1
